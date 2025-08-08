e5fe52fce8bf13fb90835836175fae17b4bfc82d
Basel <basil.omsha@gmail.com>
2025-08-08 17:27:25 +0300

    feat: add GLSL module declaration and update tsconfig to include types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,15 @@
-import { ThreeElements } from '@react-three/fiber';
+import { ThreeElements } from '@react-three/fiber'
+
+declare module '*.glsl' {
+    const content: string
+    export default content
+}
 
 declare global {
     namespace React {
         namespace JSX {
             // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-            interface IntrinsicElements extends ThreeElements { }
+            interface IntrinsicElements extends ThreeElements {}
         }
     }
 }
