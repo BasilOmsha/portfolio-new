7bc5925e6f3267a6baf97e8d822a021a37fa7918
Basel <basil.omsha@gmail.com>
2025-10-21 14:11:13 +0300

    chore: update dependencies and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 import glsl from 'vite-plugin-glsl'
+import restart from 'vite-plugin-restart'
 
 export default defineConfig({
     root: './',
     publicDir: 'public',
-    plugins: [react(), tailwindcss(), glsl()],
+    plugins: [restart({ restart: ['./public/**'] }), react(), tailwindcss(), glsl()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
